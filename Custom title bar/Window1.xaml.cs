f511246afabdf048f35f2757fe40a337ea8f5ce9
jbouchard <jbouchard@rbc.org>
2014-03-31 09:02:40 -0400

    initial

---
+++ Custom title bar/Window1.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace Custom_title_bar
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg,
+                int wParam, int lParam);
+
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public void move_window(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount==2)
+            {
+                RectangleWrapper_MouseDoubleClick(sender, e);
+            }
+            else
+            {
+            ReleaseCapture();
+            SendMessage(new WindowInteropHelper(this).Handle,
+                WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void EXIT(object sender, MouseButtonEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void MINIMIZE(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MAX_RESTORE(object sender, MouseButtonEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal) this.WindowState = WindowState.Maximized;
+            else this.WindowState = WindowState.Normal;
+        }
+
+        private void Activate_Title_Icons(object sender, MouseEventArgs e)
+        {
+            Close_btn.Fill = (ImageBrush)Main.Resources["Close_act"];
+            Min_btn.Fill = (ImageBrush)Main.Resources["Min_act"];
+            Max_btn.Fill = (ImageBrush)Main.Resources["Max_act"];
+        }
+
+        private void Deactivate_Title_Icons(object sender, MouseEventArgs e)
+        {
+            Close_btn.Fill = (ImageBrush)Main.Resources["Close_inact"];
+            Min_btn.Fill = (ImageBrush)Main.Resources["Min_inact"];
+            Max_btn.Fill = (ImageBrush)Main.Resources["Max_inact"];
+        }
+
+        private void Close_pressing(object sender, MouseButtonEventArgs e)
+        {
+            Close_btn.Fill = (ImageBrush)Main.Resources["Close_pr"];
+        }
+
+        private void Min_pressing(object sender, MouseButtonEventArgs e)
+        {
+            Min_btn.Fill = (ImageBrush)Main.Resources["Min_pr"];
+        }
+
+        private void Max_pressing(object sender, MouseButtonEventArgs e)
+        {
+            Max_btn.Fill = (ImageBrush)Main.Resources["Max_pr"];
+        }
+
+        private void RectangleWrapper_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+    }
+}
